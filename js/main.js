4a07d7d70c582200895dd8e4ae131fe3014e2871
Ashley <ashley22@vt.edu>
2020-06-16 22:07:47 -0400

    add sass, font awesome

--- js/main.js
+++ js/main.js
@@ -1,32 +1,41 @@
-var sass = require('sass');
-
-var result = sass.renderSync({
-  file: scss_filename
-});
-
 $(function() {
-$('.center').css('display', 'none');
+    $('.center').css('display', 'none');
+
+   
+//     var controller = new ScrollMagic.Controller({addIndicators: true});
+
+// // create a scene
+// var scene = new ScrollMagic.Scene({
+// 	triggerElement: "#trigger1",
+// 	triggerHook: .5,
+// 	// duration: 100, // the scene should last for a scroll distance of 100px
+// 	// offset: 50 // start this scene after scrolling for 50px
+// })
+//  // .setPin(".center") // pins the element for the the scene's duration
+	
+// 	.addIndicators()
+// 	.addTo(controller); 
+
+// scene.on("leave", function (event) {
+// $('.center').removeClass("sticky")
+
+// })
+
 });
 
+$(window).on("load", function() {
+    $(".left").animate({
+        right: '1000px'
 
-$( window ).on( "load", function() { 
-	console.log('load')
-$(".left").animate({
-                right: '1000px'
+    }, 1000, 'linear');
+    $(".right").animate({
+        left: '1000px'
 
-            },1000, 'linear');
-$(".right").animate({
-                left: '1000px'
+    }, 1000, 'linear');
 
-            },1000,'linear');
-
-setTimeout(function(){ 
-	$('.center').fadeIn(2000);
-}, 200);
+    setTimeout(function() {
+        $('.center').fadeIn(2000);
+    }, 200);
 
 
 });
-
-
-
-
