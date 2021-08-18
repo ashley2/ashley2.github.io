5b5f01c21180fd9b3a1d546195e008a2a38912f1
Ashley <ashley22@vt.edu>
2021-08-18 00:37:55 -0400

    added javascript and scss for portolfio page

---
+++ contact.js
@@ -0,0 +1,44 @@
+ $(document).ready(function() {
+
+  // airplane animation
+    const flightPath = {
+        curviness: 1.5,
+        autoRotate: true,
+        values: [
+            { x: (window.innerWidth / 8), y: -20 },
+            { x: (window.innerWidth / 8) * 2, y: 10 },
+            { x: (window.innerWidth / 8) * 3, y: 100 },
+            { x: (window.innerWidth / 8) * 4, y: -100 },
+            { x: (window.innerWidth / 8) * 3, y: -50 },
+            { x: (window.innerWidth / 8) * 5, y: 100 },
+            { x: (window.innerWidth / 8) * 7, y: 0 },
+            { x: window.innerWidth + 150, y: 100 }
+        ]
+    }
+
+    const tween = new TimelineLite()
+
+    tween.add(
+        TweenLite.to('.paper-plane', 3, {
+            bezier: flightPath,
+            ease: Power1.easeInOut
+        })
+    )
+
+    const controller = new ScrollMagic.Controller()
+    const scene = new ScrollMagic.Scene({
+            // triggerElement: '.spacer',
+            duration: 1500,
+            triggerHook: 0.5
+        })
+
+        .setTween(tween)
+        // .addIndicators()
+        .setPin('.animation')
+        .addTo(controller)
+    // end airplane animation
+
+
+
+
+});
---
+++ index.js
@@ -0,0 +1,15 @@
+ $(document).ready(function() {
+
+ // home parallax animation 
+    $(window).scroll(function(e) {
+        parallax();
+    });
+
+    function parallax() {
+        var scrolled = $(window).scrollTop();
+        $('.hero').css('top', -(scrolled * 0.0315) + 'rem');
+        $('.signature, .tagline').css('top', -(scrolled * -0.005) + 'rem');
+
+        // $('.signature, .tagline').css('opacity',1-(scrolled*.00175));
+    };
+});
