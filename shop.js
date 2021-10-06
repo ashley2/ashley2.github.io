c6541f3dcdc5a5a991b63d99059b4dad398c7a0d
Ashley <ashley22@vt.edu>
2021-10-05 21:19:23 -0400

    check in new animation or digital artxash

--- shop.js
+++ shop.js
@@ -1,75 +1,51 @@
-const tl = gsap.timeline( { 
-   scrollTrigger: {
-     trigger: ".view",
-     // start: "+=133 80%",
-     start: "top  center",
-          // end: "+=1000 ",
-      pin: true,
-     scrub: true,
-     markers: true,
-     // toggleActions: "play reverse play reverse",
-   }
-   
- });
- 
- tl.to('.slide1', { opacity: 1, duration: 0.60, y: -50 })
-   .to('.slide1', { opacity: 0, duration: 0.60,  y: -50})
-   .to('.slide2', { opacity: 1, duration: 0.60, y: -50})
-   .to('.slide2', { opacity: 0, duration: 0.60, y: -50 })
-   .to('.slide3', { opacity: 1, duration: 0.60, y: -50})
-   .to('.slide3', { opacity: 0, duration: 0.60,  y: -50})
-   .to('.slide4', { opacity: 1, duration: 0.60,  y: -50})
-   .to('.slide4', { opacity: 0, duration: 0.60, y: -50})
-
-
-// gsap.registerPlugin(ScrollTrigger);
-
-gsap.utils.toArray(".panel").forEach((panel, i) => {
-  ScrollTrigger.create({
-    trigger: ".view",
-    start: "top top", 
-    pin: true, 
-    pinSpacing: false 
-  });
-});
-
-
-// ScrollTrigger.create({
-//   snap: 1 / 4 // snap whole page to the closest section!
-// });
-
-
-// var ipad = gsap.registerPlugin(ScrollTrigger);
-
-// gsap.to(".panel:not(:last-child)", {
-//   yPercent: -100, 
-//   ease: "none",
-//   stagger: 0.5,
-//   scrollTrigger: {
-//     trigger: ".view",
-//     start: "top top",
-//     end: "+=300%",
-//     scrub: true,
-//     pin: true
-//   }
-// });
-
-
-// const imageTl = gsap.timeline({ 
+// const tl = gsap.timeline( { 
 //    scrollTrigger: {
 //      trigger: ".view",
+//      // start: "+=133 80%",
 //      start: "top  center",
+//           end: "+=4000 ",
+//       pin: true,
 //      scrub: true,
-//      markers: true,
+//      // markers: true,
 //      // toggleActions: "play reverse play reverse",
 //    }
-//    tl.to('.imageSlide1', { opacity: 1, duration: 0.60, y: -50 })
-//    .to('.imageSlide1', { opacity: 0, duration: 0.60,  y: -50})
-//    .to('.imageSlide2', { opacity: 1, duration: 0.60, y: -50})
-//    .to('.imageSlide2', { opacity: 0, duration: 0.60, y: -50 })
+   
+//  });
+ 
+//  tl.to('.slide1', { opacity: 1, duration: 0.60, y: -50 })
+//    .to('.slide1', { opacity: 0, duration: 0.60,  y: -50})
+//    .to('.slide2', { opacity: 1, duration: 0.60, y: -50})
+//    .to('.slide2', { opacity: 0, duration: 0.60, y: -50 })
 //    .to('.slide3', { opacity: 1, duration: 0.60, y: -50})
 //    .to('.slide3', { opacity: 0, duration: 0.60,  y: -50})
 //    .to('.slide4', { opacity: 1, duration: 0.60,  y: -50})
 //    .to('.slide4', { opacity: 0, duration: 0.60, y: -50})
-//  });
-   
+
+
+const ipadTl = gsap.timeline({
+scrollTrigger: {
+     trigger: ".ipads",
+     start: 150,
+          end: myLeaveFunc,
+      // pin: true,
+     scrub: true,
+     markers: true,
+     // toggleActions: "play reverse play reverse",
+   }
+   })
+ipadTl.to('.ipadLeft', { opacity: 1, duration: 1,  x: -300})
+.to('.ipadRight', { opacity: 1, duration: 1,  x: 300}, "<")
+
+
+ScrollTrigger.create({
+  trigger: elem,
+  onEnter: myEnterFunc,
+  onLeave: myLeaveFunc,
+  onEnterBack: myEnterFunc,
+  onLeaveBack: myLeaveFunc
+});
+
+
+function myLeaveFunc(){
+  console.log("test")
+}
