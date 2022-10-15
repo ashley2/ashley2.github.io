ebc2f1464a32fd0633281dcaf8b0d72bc6821f3e
Ashley <ashley22@vt.edu>
2022-10-15 17:34:09 -0400

    navbar bg bug

--- main.js
+++ main.js
@@ -7,6 +7,7 @@
 
 $(document).ready(function() {
 
+let navMain =  $('#navMain');
     // home parallax animation 
     $(window).scroll(function(e) {
         parallax();
@@ -20,6 +21,18 @@ $(document).ready(function() {
         // $('.signature, .tagline').css('opacity',1-(scrolled*.00175));
     };
 
+window.addEventListener('resize', function(event){
+console.log('resizing')
+let windoWidth = document.documentElement.clientWidth;
+if (windoWidth >= 992) {
+    console.log('992+')
+$('#overlay').removeClass('overlay');
+ $('#navMain').addClass('bg-transparent navbar-light');
+ $('#navMain').removelass(' navbar-dark');
+
+  // document.querySelector('.height').innerText = document.documentElement.clientHeight;
+}
+});
 
 
 });
@@ -37,19 +50,19 @@ function navFunction() {
     console.log(pathname)
     switch (pathname) {
         case 'work/':
-            $('#navMain').toggleClass('bg-transparent')
+            $(navMain).toggleClass('bg-transparent')
             overlayScroll();
             break;
         case 'contact/':
-            $('#navMain').toggleClass('bg-transparent')
+            $(navMain).toggleClass('bg-transparent')
             overlayScroll();
             break;
         case 'about/':
-            $('#navMain').toggleClass('bg-transparent navbar-dark navbar-light');
+            $(navMain).toggleClass('bg-transparent navbar-dark navbar-light');
             overlayScroll();
             break;
         default:
-            $('#navMain').toggleClass('bg-transparent navbar-dark navbar-light');
+            $(navMain).toggleClass('bg-transparent navbar-dark navbar-light');
             overlayScroll();
     }
 
@@ -57,7 +70,7 @@ function navFunction() {
     $('.dropdown-item').click(function() {
         overlayScroll();
         $('.navbar-collapse').removeClass("show");
-        $('#navMain').toggleClass('bg-transparent')
+        $(navMain).toggleClass('bg-transparent')
 
 
     })
