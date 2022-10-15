358335105d2fc0106b431d435fef3b904c076d6e
Ashley <ashley22@vt.edu>
2022-10-14 21:34:00 -0400

    add accessibility, skip to main, logo on nav, responsiveness

--- main.js
+++ main.js
@@ -5,23 +5,7 @@
 //   });
 
 
-
-// $(document).ready(function() {
-// /*! Fades in page on load */
-// $('body').css('display', 'none').fadeIn(500);
-// console.log('fade')
-// function isMobile() {
-//   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-// }
-
-// });
-
 $(document).ready(function() {
-
-    // setTimeout(function() { 
-    //        $('#homeLink').addClass('active');
-    //  }, 500);
-
 
     // home parallax animation 
     $(window).scroll(function(e) {
@@ -39,26 +23,31 @@ $(document).ready(function() {
 
 
 });
-   let pathname = window.location.pathname.slice(1).toString()
+
+function overlayScroll() {
+    $('#overlay').toggleClass('overlay');
+    $('body').toggleClass('stop-scrolling');
+}
+
+
+
+let pathname = window.location.pathname.slice(1).toString()
+
 function navFunction() {
     if (pathname === "work.html") {
-        console.log('2')
-         $('#navMain').toggleClass('bg-transparent')
-        $('#overlay').toggleClass('overlay');
-        $('body').toggleClass('stop-scrolling');
+        $('#navMain').toggleClass('bg-transparent')
+        overlayScroll();
     } else {
         $('#navMain').toggleClass('bg-transparent navbar-dark navbar-light');
-        $('#overlay').toggleClass('overlay');
-        $('body').toggleClass('stop-scrolling');
-
+        overlayScroll();
     }
 
-    // let pathname = window.location.pathname.slice(1).toString()
-    // console.log(pathname)
+    $('.dropdown-item').click(function() {
+        overlayScroll();
+        $('.navbar-collapse').removeClass("show");
+        $('#navMain').toggleClass('bg-transparent')
 
-    // if (pathname === "work.html") {
-    //     $('#workLink').html('<li class="nav-item text-center dropdown"><a class="nav-link dropdown-toggle active" href="#" id="portfolioDropDown" role="button" data-bs-toggle="dropdown" aria-expanded="false" aria-current="page">work</a><ul class="dropdown-menu" aria-labelledby="portfolioDropDown"><li><a class="dropdown-item" href="#panelOne">Web Development & Design</a></li><li><a class="dropdown-item" href="#panelTwo">Mockups & Design</a></li> <li><a class="dropdown-item" href="#panelThree">Logos & Branding</a></li><li><a class="dropdown-item" href="#panelFour">Digital Art</a></li></ul></li>')
-    //     $('#navMain').toggleClass('navbar-light')
-    // }
+
+    })
 
 }
